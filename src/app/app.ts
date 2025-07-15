b2feee12f0ba7bf068665153607f76939e41a1d4
SYSTEX\2500430 <jeff_lee@systex.com>
2025-07-15 17:27:46 +0800

    新增: 【2.基礎語法篇】Angular 入門教學(2_7) - 利用ngFor提供的index參數刪除陣列裡的指定物件

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,54 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'myApp';
+  title = 'myApp';
+  placeholder = 'What needs to be done?'
+
+  toggleAllBtn = false;
+  check1 = false;
+  check2 = false;
+
+  todoDataList = [
+    {
+      Status: true,
+      Thing: '第一件事'
+    },
+    {
+      Status: false,
+      Thing: '第二件事'
+    },
+    {
+      Status: true,
+      Thing: '第三件事'
+    }
+  ]
+
+  toggleAll() {
+    this.toggleAllBtn = !this.toggleAllBtn;
+    this.check1 = this.toggleAllBtn;
+    this.check2 = this.toggleAllBtn;
+  }
+
+  clickCheck(item: any) {
+    item.Status = !item.Status
+  }
+
+  delete(index: number) {
+    this.todoDataList.splice(index, 1)
+  }
+
+  add(event: KeyboardEvent, input: HTMLInputElement) {
+    if (event.key === 'Enter') {
+      const value = (event.target as HTMLInputElement).value;
+      this.todoDataList.push({
+        Status: false,
+        Thing: input.value
+      });
+      input.value = '';
+    }
+  }
 }
