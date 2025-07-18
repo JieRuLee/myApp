43a9045033eb8e6d23c1bdf4628047db256bc1c7
SYSTEX\2500430 <jeff_lee@systex.com>
2025-07-18 17:38:00 +0800

    新增:【3.HttpClient與後端溝通】Angular 入門教學(3_5) - 利用HttpClient刪除資料至後端伺服器

--- src/app/@models/todo. model.ts
+++ src/app/@models/todo. model.ts
@@ -1,16 +1,21 @@
 export interface Todo {
     Status: boolean;
-    Thing: String;
+    Thing: string;
     Editing: boolean;
+    TodoId: string;
 }
 
 export class TodoClass {
     Status: boolean;
     Thing: String;
+    Editing: boolean;
+    TodoId: string;
 
     constructor(_thiing: string, _status: boolean = false) {
         this.Thing = _thiing;
         this.Status = _status;
+        this.Editing =  false;
+        this.TodoId = '';
     }
     toggle() {
         this.Status = !this.Status;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,82 +1,94 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Todo, TodoClass, TodoStatusType } from './@models/todo. model';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  imports: [FormsModule],
 })
+
 export class App {
   title = 'myApp';
   placeholder = 'What needs to be done?'
   toggleAllBtn = false;
   nowTodoStatusType = TodoStatusType.All;
   TodoStatusType = TodoStatusType;
+  todoInputModel = '';
 
-  todoDataList: Todo[] = [
-    {
-      Status: true,
-      Thing: '第一件事',
-      Editing: false
-    },
-    {
-      Status: false,
-      Thing: '第二件事',
-      Editing: false
-    },
-    {
-      Status: true,
-      Thing: '第三件事',
-      Editing: false
-    }
-  ]
+  todoDataList: Todo[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getData()
+  }
+
+  getData(){
+    this.http.get<Todo[]>('/api/todo2_16').subscribe(data => {
+      this.todoDataList = data
+    });
+  }
 
   toggleAll() {
     this.toggleAllBtn = !this.toggleAllBtn;
-    this.todoDataList.forEach(data =>{
+    this.todoDataList.forEach(data => {
       data.Status = this.toggleAllBtn;
-    })
+    });
+
+    this.http.put('/api/todo2_16/Status/' + this.toggleAllBtn, null).subscribe();
   }
 
   clickCheck(item: Todo) {
     item.Status = !item.Status;
-    if(this.todoCompleted.length === this.todoDataList.length){
+    this.http.put('/api/todo2_16/'+ item.TodoId, item).subscribe();
+    this.checkToggleAllBtn()
+  }
+
+  checkToggleAllBtn(){
+    if (this.todoCompleted.length === this.todoDataList.length) {
       this.toggleAllBtn = true;
     } else {
       this.toggleAllBtn = false;
     }
   }
 
-  delete(todo: Todo) {
-    this.todoDataList = this.todoDataList.filter(data => data !== todo)
-    console.log(this.todoDataList)
+  delete(item: Todo) {
+    this.http.delete('/api/todo2_16/' + item.TodoId).subscribe();
+    this.todoDataList = this.todoDataList.filter(data => data !== item)
   }
 
-  add(value: string) {
-    const todo: Todo = {
-      Status: false,
-      Thing: value,
-      Editing: false
-    }
-    this.todoDataList.push(todo)
-  }
-
-  edit(item: Todo){
-    item.Editing = true;
-  }
-
-  update(item: Todo, value: string){
-    item.Thing = value;
+  update(item: Todo){
+    this.http.put('/api/todo2_16/'+ item.TodoId, item).subscribe();
     item.Editing = false;
   }
 
-  setTodoStatusType(type: number){
+  add() {
+    const todo: Todo = {
+      Status: false,
+      Thing: this.todoInputModel,
+      Editing: false,
+      TodoId: '',
+    }
+    this.http.post<Todo>('/api/todo2_16', todo).subscribe((data) => {
+      this.todoDataList.push(data);
+    });
+    this.todoInputModel = '';
+  }
+
+  edit(item: Todo) {
+    item.Editing = true;
+  }
+
+  setTodoStatusType(type: TodoStatusType) {
     this.nowTodoStatusType = type;
   }
 
-  get nowToddoList(): Todo[]{
-    let list : Todo[] = [];
+  get nowTodoList(): Todo[] {
+    let list: Todo[] = [];
     switch (this.nowTodoStatusType) {
       case TodoStatusType.Active:
         list = this.todoActive;
@@ -93,15 +105,16 @@ export class App {
   }
 
 
-  get todoActive(): Todo[]{
+  get todoActive(): Todo[] {
     return this.todoDataList.filter(data => !data.Status)
   }
 
-  get todoCompleted(): Todo[]{
+  get todoCompleted(): Todo[] {
     return this.todoDataList.filter(data => data.Status)
   }
 
-  clearCompleted(){
+  clearCompleted() {
+    this.http.delete('/api/todo2_16/clearCompleted').subscribe();
     this.todoDataList = this.todoActive;
   }
 }
